3ee9d70f61374bf89865e2c15e16e350a7ce62a0
chenmin <472677037@qq.com>
2017-06-06 10:38:59 +0800

    v1.0.0

---
+++ app/src/main/java/com/chm/chmframwork/WelcomeActivity.java
@@ -0,0 +1,56 @@
+package com.chm.chmframwork;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * 欢迎页
+ * Created by chenmin on 2017/6/5.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+    private Toolbar mToolBar;
+    private TextView mTvBtnFlow, mTvBtnWechat, mTvBtnZhihu;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        initView();
+    }
+
+    private void initView() {
+        mToolBar = (Toolbar) findViewById(R.id.toolbar);
+        mTvBtnFlow = (TextView) findViewById(R.id.tv_btn_flow);
+        mTvBtnWechat = (TextView) findViewById(R.id.tv_btn_wechat);
+        mTvBtnZhihu = (TextView) findViewById(R.id.tv_btn_zhihu);
+
+        setSupportActionBar(mToolBar);
+
+        mTvBtnFlow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, com.chm.chmframwork.demo_flow.MainActivity.class));
+            }
+        });
+
+        mTvBtnWechat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, com.chm.chmframwork.demo_wechat.MainActivity.class));
+            }
+        });
+
+        mTvBtnZhihu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WelcomeActivity.this, com.chm.chmframwork.demo_zhihu.MainActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/chm/chmframwork/ui/fragment/MainFragment.java
@@ -0,0 +1,56 @@
+package com.chm.chmframwork.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chm.chmframwork.R;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import me.yokeyword.fragmentation.SupportFragment;
+
+/**
+ * Created by YoKeyword on 16/6/30.
+ */
+public class MainFragment extends SupportFragment {
+    private static final int REQ_MSG = 10;
+
+    public static final int FIRST = 0;
+    public static final int SECOND = 1;
+    public static final int THIRD = 2;
+
+    private SupportFragment[] mFragments = new SupportFragment[3];
+
+
+    public static MainFragment newInstance() {
+        Bundle args = new Bundle();
+        MainFragment fragment = new MainFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+
+    }
+
+    @Override
+    protected void onFragmentResult(int requestCode, int resultCode, Bundle data) {
+        super.onFragmentResult(requestCode, resultCode, data);
+        if (requestCode == REQ_MSG && resultCode == RESULT_OK) {
+
+        }
+    }
+
+}
