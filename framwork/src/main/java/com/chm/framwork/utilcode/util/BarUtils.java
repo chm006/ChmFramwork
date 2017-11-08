d765a18a636cbfe1d37b449a0935acd29cdf2018
chenmin <472677037@qq.com>
2017-11-08 16:34:18 +0800

    lint警告修复（framwork）

--- framwork/src/main/java/com/chm/framwork/utilcode/util/BarUtils.java
+++ framwork/src/main/java/com/chm/framwork/utilcode/util/BarUtils.java
@@ -16,8 +16,6 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 
-import java.lang.reflect.Method;
-
 /**
  * <pre>
  *     author: Blankj
@@ -28,10 +26,10 @@ import java.lang.reflect.Method;
  */
 public class BarUtils {
 
-    private static final int    DEFAULT_STATUS_BAR_ALPHA  = 112;
-    private static final String FAKE_STATUS_BAR_VIEW_TAG  = "FAKE_STATUS_BAR_VIEW_TAG";
+    private static final int DEFAULT_STATUS_BAR_ALPHA = 112;
+    private static final String FAKE_STATUS_BAR_VIEW_TAG = "FAKE_STATUS_BAR_VIEW_TAG";
     private static final String FAKE_TRANSLUCENT_VIEW_TAG = "FAKE_TRANSLUCENT_VIEW_TAG";
-    private static final int    TAG_KEY_HAVE_SET_OFFSET   = -123;
+    private static final int TAG_KEY_HAVE_SET_OFFSET = -123;
 
     private BarUtils() {
         throw new UnsupportedOperationException("u can't instantiate me...");
@@ -733,46 +731,5 @@ public class BarUtils {
             return TypedValue.complexToDimensionPixelSize(tv.data, activity.getResources().getDisplayMetrics());
         }
         return 0;
-    }
-
-    /**
-     * 显示通知栏
-     * <p>需添加权限 {@code <uses-permission android:name="android.permission.EXPAND_STATUS_BAR"/>}</p>
-     *
-     * @param context        上下文
-     * @param isSettingPanel {@code true}: 打开设置<br>{@code false}: 打开通知
-     */
-    public static void showNotificationBar(Context context, boolean isSettingPanel) {
-        String methodName = (Build.VERSION.SDK_INT <= 16) ? "expand"
-                : (isSettingPanel ? "expandSettingsPanel" : "expandNotificationsPanel");
-        invokePanels(context, methodName);
-    }
-
-    /**
-     * 隐藏通知栏
-     * <p>需添加权限 {@code <uses-permission android:name="android.permission.EXPAND_STATUS_BAR"/>}</p>
-     *
-     * @param context 上下文
-     */
-    public static void hideNotificationBar(Context context) {
-        String methodName = (Build.VERSION.SDK_INT <= 16) ? "collapse" : "collapsePanels";
-        invokePanels(context, methodName);
-    }
-
-    /**
-     * 反射唤醒通知栏
-     *
-     * @param context    上下文
-     * @param methodName 方法名
-     */
-    private static void invokePanels(Context context, String methodName) {
-        try {
-            Object service = context.getSystemService("statusbar");
-            Class<?> statusBarManager = Class.forName("android.app.StatusBarManager");
-            Method expand = statusBarManager.getMethod(methodName);
-            expand.invoke(service);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
